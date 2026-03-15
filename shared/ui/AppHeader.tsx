a48ef7e326c8a710b4026c0d85714fefb7476a2b
parkchaeyeon <pcy6904@naver.com>
2026-03-15 22:03:59 +0900

    style : AppHeader icon 추가

--- shared/ui/AppHeader.tsx
+++ shared/ui/AppHeader.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { Calendar, Bell, CircleUser } from "lucide-react";
 import { usePathname } from "next/navigation";
 
 const titles: Record<string, string> = {
@@ -16,10 +16,16 @@ const AppHeader = () => {
       <div className="mx-auto flex h-full max-w-md items-center justify-between">
         <h1 className="text-lg font-semibold">{titles[pathname]}</h1>
 
-        <div className="flex gap-4">
-          <div>달력</div>
-          <div>종</div>
-          <div>마이페이지</div>
+        <div className="flex gap-5">
+          <div>
+            <Calendar size={20} />
+          </div>
+          <div>
+            <Bell size={20} />
+          </div>
+          <div>
+            <CircleUser size={20} />
+          </div>
         </div>
       </div>
     </header>
