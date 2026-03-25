da5a27ba9836da125fab4e91dcf21b2407145a90
parkchaeyeon <pcy6904@naver.com>
2026-03-25 23:33:13 +0900

    feature : ClothesUpload page 2차 레이아웃 구현

--- app/clothes/new/page.tsx
+++ app/clothes/new/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import AccordionSelectedField from "../components/AccordionSelectedField";
-import Accordion from "../components/Accordion";
+import Accordion from "../../../shared/ui/accordion/Accordion";
 
 const SEASON = ["봄", "여름", "가을", "겨울"];
 const TPO = [
@@ -41,8 +41,17 @@ const ClothesUpload = () => {
     tpo: false as boolean,
     category: false as boolean,
     brand: false as boolean,
+    purchase: false as boolean,
+    memo: false as boolean,
   });
   const [brand, setBrand] = useState("");
+  const [memo, setMemo] = useState("");
+  const [purchaseInfo, setPurchaseInfo] = useState({
+    date: "" as string,
+    price: "" as string | number,
+    link: "" as string,
+    product_code: "" as string,
+  });
 
   type SectionField = keyof typeof openSection;
   type SelectField = keyof typeof selectedMap;
@@ -108,6 +117,56 @@ const ClothesUpload = () => {
             className="w-full rounded border px-3 py-2"
             placeholder="브랜드를 입력해주세요."
           />
+        </Accordion>
+        <Accordion
+          title={"구매"}
+          onClick={() => handleToggleSection("purchase")}
+          selected={Object.values(purchaseInfo).filter(Boolean).join(",")}
+          isOpen={openSection.purchase}
+        >
+          <input
+            value={purchaseInfo.price}
+            onChange={(e) =>
+              setPurchaseInfo((prev) => ({ ...prev, price: e.target.value }))
+            }
+            className="w-full rounded border px-3 py-2"
+            placeholder="가격"
+          />
+          <input
+            value={purchaseInfo.date}
+            onChange={(e) =>
+              setPurchaseInfo((prev) => ({ ...prev, date: e.target.value }))
+            }
+            className="w-full rounded border px-3 py-2"
+            placeholder="구매일"
+          />
+          <input
+            value={purchaseInfo.link}
+            onChange={(e) =>
+              setPurchaseInfo((prev) => ({ ...prev, link: e.target.value }))
+            }
+            className="w-full rounded border px-3 py-2"
+            placeholder="구매 링크"
+          />
+          <input
+            value={purchaseInfo.product_code}
+            onChange={(e) =>
+              setPurchaseInfo((prev) => ({
+                ...prev,
+                product_code: e.target.value,
+              }))
+            }
+            className="w-full rounded border px-3 py-2"
+            placeholder="코드"
+          />
+        </Accordion>
+        <Accordion
+          title={"메모"}
+          onClick={() => handleToggleSection("memo")}
+          selected={memo}
+          isOpen={openSection.memo}
+        >
+          <textarea className="w-full h-[100px] rounded border px-3 py-2"></textarea>
         </Accordion>
       </form>
     </div>
